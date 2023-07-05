8131b0d30e716f0c079d5ec4e793baa2a9e5ebd3
Arturo <41492101+Arti-Code@users.noreply.github.com>
2023-07-05 19:21:39 +0200

    v0.4.4

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod motor;
 
 use clap::Parser;
 use anyhow::Result;
-use webrtc::ice_transport::ice_credential_type::RTCIceCredentialType;
+//use webrtc::ice_transport::ice_credential_type::RTCIceCredentialType;
 use std::sync::Arc;
 use std::time::Duration;
 use std::env;
+use std::thread;
 use tokio::time::sleep;
 use tokio::net::{UdpSocket};
 use serde::{Serialize, Deserialize};
@@ -25,23 +26,15 @@ use webrtc::rtp_transceiver::rtp_codec::RTCRtpCodecCapability;
 use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
 use webrtc::track::track_local::{TrackLocal, TrackLocalWriter};
 use webrtc::Error;
-//use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_VP8};
 use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_H264};
-//use webrtc::rtp_transceiver::rtp_sender;
-//use webrtc::track::track_local::track_local_static_sample::TrackLocalStaticSample;
-use rppal::gpio::*;
 use crate::motor::*;
+
+
 
 //const DB_REF: &str = "https://rtp-to-webrtc-default-rtdb.firebaseio.com";
 const DB_REF: &str = "https://my-killer-bot-default-rtdb.europe-west1.firebasedatabase.app";
-const DEVICE: &str = "tankor";
+//const DEVICE: &str = "commander";
 
-#[derive(Parser, Debug)]
-#[command(device)]
-struct Arguments {
-    #[arg(short, long, default_value_t="tankor")]
-    device: String,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Answer {
@@ -52,6 +45,13 @@ struct Answer {
 struct Offer {
     offer: String
 }
+
+#[derive(Parser, Debug)]
+struct Arguments {
+    #[arg(short, long, default_value_t=String::from("autobot"))]
+    device: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Device {
     name: String
@@ -71,12 +71,21 @@ impl Device {
 fn main() {
     let args = Arguments::parse();
     loop {
-        run(args.device).unwrap();
+        match run(args.device.clone()) {
+            Ok(restart) if restart => {
+                println!("[MAIN PROCESS]: restart");
+                thread::sleep(Duration::from_secs(1));
+                continue;
+            },
+            Ok(restart) if !restart => {break;},
+            Ok(_) => {break;},
+            Err(_) => {break;}
+        }
     }
 }
 
 #[tokio::main]
-async fn run(device_name: String) -> Result<()> {
+async fn run(device_name: String) -> Result<bool> {
     prog_intro().await;
     let listener = create_video_listener().await;
     let device: Device = create_device(&device_name).await;
@@ -143,23 +152,22 @@ async fn run(device_name: String) -> Result<()> {
     }));
 
     tokio::spawn(async move {
-        let mut motors = new_motors();
-        prepare(&mut motors);
+        let mut motors = Motors::new();
+        motors.prepare();
         while let Some(cmd) = drive_rx.recv().await {
             if cmd=="front" {
-                front(&mut motors);
+                motors.front();
             } else if cmd=="stop" {
-                stop(&mut motors);
+                motors.stop();
             } else if cmd=="back" {
-                back(&mut motors);
+                motors.back();
             } else if cmd=="left" {
-                left(&mut motors);
+                motors.left();
             } else if cmd=="right" {
-                right(&mut motors);
+                motors.right();
             }
         }
-        finish(&mut motors);
-        println!("[MOTORS]: STOPPED");
+        motors.finish();
     });
 
     peer_connection.on_ice_connection_state_change(Box::new(move |connection_state: RTCIceConnectionState| {
@@ -176,7 +184,7 @@ async fn run(device_name: String) -> Result<()> {
     let done_tx2 = done_tx.clone();
 
     peer_connection.on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
-        println!("[PEER CONNECTION]: {}", s);
+        println!("<y>[PEER CONNECTION]: {}</>", s);
         if s == RTCPeerConnectionState::Failed {
             let _ = done_tx2.try_send(());
         }
@@ -196,13 +204,13 @@ async fn run(device_name: String) -> Result<()> {
     if let Some(local_desc) = peer_connection.local_description().await {
         send_answer(&local_desc, device.get_name()).await;
     } else {
-        println!("[ANSWER]: failed!");
+        println!("<r>[ANSWER]: failed!</>");
     }
     let done_tx3 = done_tx.clone();
     
     let l = Arc::clone(&listener);
     tokio::spawn(async move {
-        let mut inbound_rtp_packet = vec![0u8; 4096]; // UDP MTU
+        let mut inbound_rtp_packet = vec![0u8; 4096];
         while let Ok((n, _)) = l.recv_from(&mut inbound_rtp_packet).await {
             if let Err(err) = video_track.write(&inbound_rtp_packet[..n]).await {
                 if Error::ErrClosedPipe == err {
@@ -214,20 +222,21 @@ async fn run(device_name: String) -> Result<()> {
             }
         }
     });
-    
+    let mut restart: bool=true;
     tokio::select! {
         _ = done_rx.recv() => {
             println!("[PROCESS]: done");
         }
         _ = tokio::signal::ctrl_c() => {
             println!("[PROCESS]: CTRL+C by user");
+            restart=false;
         }
     };
 
     peer_connection.remove_track(&rtp_sender2).await.unwrap();
     peer_connection.close().await?;
     println!("[CONNECTION] close");
-    Ok(())
+    Ok(restart)
 }
 
 async fn wait(seconds: u32) {
@@ -257,17 +266,17 @@ async fn create_video_listener() -> Arc<UdpSocket> {
 
 async fn create_rtcconfig() -> RTCConfiguration {
     RTCConfiguration {
-        /* ice_servers: vec![RTCIceServer {
+        ice_servers: vec![RTCIceServer {
             urls: vec!["stun:stun.l.google.com:19302".to_owned()],
             ..Default::default()
-        }], */
+        }],
 
-        ice_servers: vec![RTCIceServer {
+        /* ice_servers: vec![RTCIceServer {
             urls: vec!["stun:fr-turn1.xirsys.com".to_owned()],
             username: "23Xgr3XVCOk2GqoZW5eWhbdXM1EfA8VcC6OVVacJSpFdoljTUOsTcgAoFUvfN4vcAAAAAGNFN29nd296ZHlrMg==".to_owned(),
             credential: "2ed490ce-4947-11ed-bd3d-0242ac120004".to_owned(),
             credential_type: RTCIceCredentialType::default(),
-        }],
+        }], */
         ..Default::default()
     }
 }
@@ -292,13 +301,14 @@ async fn wait_offer(device: &str) -> String {
         .unwrap().at("signaling").at(&device).at("offer");
     let mut offer_founded: bool=false;
     let mut offer_b64: String=String::new();
-    println!("WAITING FOR CONNECTION...");
+    println!("[CONNECTION]: waiting...");
     sleep(Duration::from_secs(1)).await;
-    
+    let mut t = 1;
     while !offer_founded {
         let encod = firebase.get::<String>().await;
         match encod  {
             Ok(v) if v != "" => {
+                println!("wait: {}", v);
                 offer_b64 = v;
                 offer_founded = true;
                 let firebase2 = Firebase::new(DB_REF)
@@ -307,14 +317,19 @@ async fn wait_offer(device: &str) -> String {
                 firebase2.update(&clear_offer).await.unwrap();
             },
             Ok(_) => {
-                sleep(Duration::from_secs(1)).await;
+                println!("waiting...\t({}s)", t);
+                sleep(Duration::from_secs(t as u64)).await;
+                if t < 45 {
+                    t += 1;
+                }
             },
-            Err(_) => {
+            Err(e) => {
+                println!("wait: Err: {}", e);
                 sleep(Duration::from_secs(3)).await;
             }
         }
     }
-    println!("[OFFER]: Ok");
+    println!("[OFFER]: founded {}s", t);
     offer_b64
 }
 
--- src/motor.rs
+++ src/motor.rs
@@ -1,105 +1,31 @@
-//use std::error::Error;
-//use std::thread;
-//use std::time::Duration;
 use rppal::gpio::{Gpio, OutputPin};
-//use rppal::pwm::{Channel, Polarity, Pwm};
-use std::clone::Clone;
 
-const PWMA: u8 = 16;
-const AIN1: u8 = 21;
-const AIN2: u8 = 20;
+//? const PWMA: u8 = 16;
+//? const AIN1: u8 = 21;
+//? const AIN2: u8 = 20;
+//? const PWMB: u8 = 26;
+//? const BIN1: u8 = 13;
+//? const BIN2: u8 = 19;
+//? const STBY: u8 = 6;
 
-const PWMB: u8 = 26;
-const BIN1: u8 = 13;
-const BIN2: u8 = 19;
+//* const PWMA: u8 = 6;
+//* const AIN1: u8 = 19;
+//* const AIN2: u8 = 13;
+//* const PWMB: u8 = 16;
+//* const BIN1: u8 = 21;
+//* const BIN2: u8 = 20;
+//* const STBY: u8 = 26;
 
-const STBY: u8 = 6;
-
+const PWMA: u8 = 6;
+const AIN1: u8 = 13;
+const AIN2: u8 = 19;
+const PWMB: u8 = 12;
+const BIN1: u8 = 16;
+const BIN2: u8 = 20;
+const STBY: u8 = 26;
 
 pub struct Motors {
-    gpio: Gpio,
-    pwma: OutputPin,
-    ain1: OutputPin,
-    ain2: OutputPin,
-    pwmb: OutputPin,
-    bin1: OutputPin,
-    bin2: OutputPin,
-    stby: OutputPin,
-}
-
-pub fn new_motors() -> Motors {
-    let gpio = Gpio::new().unwrap();
-    let ain1 = gpio.get(AIN1).unwrap().into_output();
-    let ain2 = gpio.get(AIN2).unwrap().into_output();
-    let pwma = gpio.get(PWMA).unwrap().into_output();
-    let bin1 = gpio.get(BIN1).unwrap().into_output();
-    let bin2 = gpio.get(BIN2).unwrap().into_output();
-    let stby = gpio.get(STBY).unwrap().into_output();
-    let pwmb = gpio.get(PWMB).unwrap().into_output();
-    Motors { gpio: gpio, pwma: pwma, ain1: ain1, ain2: ain2, pwmb: pwmb, bin1: bin1, bin2: bin2, stby: stby }
-}
-
-pub fn stop(motors: &mut Motors) {
-    motors.ain1.set_low();
-    motors.ain2.set_low();
-    motors.bin1.set_low();
-    motors.bin2.set_low();
-    println!("STOP @inside");
-}    
-
-pub fn right(motors: &mut Motors) {
-    motors.ain1.set_high();
-    motors.ain2.set_low();
-    motors.bin1.set_high();
-    motors.bin2.set_low();
-    println!("RIGHT @inside");
-}
-
-pub fn left(motors: &mut Motors) {
-    motors.ain1.set_low();
-    motors.ain2.set_high();
-    motors.bin1.set_low();
-    motors.bin2.set_high();
-    println!("LEFT @inside");
-}
-
-pub fn front(motors: &mut Motors) {
-    motors.ain1.set_high();
-    motors.ain2.set_low();
-    motors.bin1.set_low();
-    motors.bin2.set_high();
-    println!("FRONT @inside");
-}
-
-pub fn back(motors: &mut Motors) {
-    motors.ain1.set_low();
-    motors.ain2.set_high();
-    motors.bin1.set_high();
-    motors.bin2.set_low();
-    println!("BACK @inside");
-}
-
-pub fn finish(motors: &mut Motors) {
-    _ = motors.pwma.clear_pwm();
-    _ = motors.pwmb.clear_pwm();
-    motors.stby.set_low();
-    println!("FINISH @inside");
-}
-
-pub fn prepare(motors: &mut Motors) {
-    _ = motors.pwma.set_pwm_frequency(50.0, 1.0);
-    _ = motors.pwmb.set_pwm_frequency(50.0, 1.0);
-    motors.ain1.set_low();
-    motors.ain2.set_low();
-    motors.bin1.set_low();
-    motors.bin2.set_low();
-    motors.stby.set_high();
-    println!("PREPARE @inside");
-}
-
-//#[derive(Clone)]
-/* pub struct Motors {
-    gpio: Gpio,
+    // gpio: Gpio,
     pwma: OutputPin,
     ain1: OutputPin,
     ain2: OutputPin,
@@ -119,7 +45,7 @@ impl Motors {
         let bin2 = gpio.get(BIN2).unwrap().into_output();
         let stby = gpio.get(STBY).unwrap().into_output();
         let pwmb = gpio.get(PWMB).unwrap().into_output();
-        Motors { gpio: gpio, pwma: pwma, ain1: ain1, ain2: ain2, pwmb: pwmb, bin1: bin1, bin2: bin2, stby: stby }
+        Motors { pwma: pwma, ain1: ain1, ain2: ain2, pwmb: pwmb, bin1: bin1, bin2: bin2, stby: stby }
     }
 
     pub fn prepare(&mut self) {
@@ -129,40 +55,38 @@ impl Motors {
         self.ain2.set_low();
         self.bin1.set_low();
         self.bin2.set_low();
-        self.stby.set_low();
+        self.stby.set_high();
     }
 
     pub fn stop(&mut self) {
-        println!("stop inside");
         self.ain1.set_low();
         self.ain2.set_low();
         self.bin1.set_low();
         self.bin2.set_low();
     }    
 
-    pub fn front(&mut self) {
-        println!("front inside");
+    pub fn back(&mut self) {
         self.ain1.set_high();
         self.ain2.set_low();
         self.bin1.set_high();
         self.bin2.set_low();
     }
 
-    pub fn back(&mut self) {
+    pub fn front(&mut self) {
         self.ain1.set_low();
         self.ain2.set_high();
         self.bin1.set_low();
         self.bin2.set_high();
     }
 
-    pub fn left(&mut self) {
+    pub fn right(&mut self) {
         self.ain1.set_high();
         self.ain2.set_low();
         self.bin1.set_low();
         self.bin2.set_high();
     }
 
-    pub fn right(&mut self) {
+    pub fn left(&mut self) {
         self.ain1.set_low();
         self.ain2.set_high();
         self.bin1.set_high();
@@ -170,10 +94,10 @@ impl Motors {
     }
 
     pub fn finish(&mut self) {
-        self.pwma.clear_pwm();
-        self.pwmb.clear_pwm();
+        _ = self.pwma.clear_pwm();
+        _ = self.pwmb.clear_pwm();
         self.stby.set_low();
     }
 
 
-} */
+}
