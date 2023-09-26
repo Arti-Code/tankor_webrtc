761a5184377187454009b59b7a632b21aa91ce18
Arturo <41492101+Arti-Code@users.noreply.github.com>
2023-09-26 15:44:07 +0200

    v0.4.1a

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod emulator;
+mod motor;
 
 use clap::Parser;
 use anyhow::Result;
@@ -28,7 +28,7 @@ use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
 use webrtc::track::track_local::{TrackLocal, TrackLocalWriter};
 use webrtc::Error;
 use webrtc::api::media_engine::{MediaEngine, /* MIME_TYPE_H264 */};
-use crate::emulator::*;
+use crate::motor::*;
 
 
 
